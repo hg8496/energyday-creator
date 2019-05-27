4828b7090d19a82dd946fb8e3a42b4fa6235ae27
Christian Stolz <hg8496@cstolz.de>
2019-05-27 22:24:02 +0200

    some code restructuring

--- lib/cli.ts
+++ lib/cli.ts
@@ -6,9 +6,8 @@ import * as fs from "fs";
 import * as moment from "moment";
 import { Writer } from "protobufjs";
 import { findDevice } from "./index";
-import * as mapper from "./mapper";
+import { doesMap, mapToValueStream } from "./mapper";
 import EnergyDay = janitza.values.EnergyDay;
-import ValueStream = janitza.values.ValueStream;
 
 commander
     .version("1.0.0")
@@ -30,10 +29,7 @@ async function main() {
     if (prjDevice) {
         const { project, device } = prjDevice;
         const values = await client.values.list(project, device);
-        const knownValues = values.filter(
-            value =>
-                mapper.mapGridVisToProtoBuffer(value.valueType.value) && mapper.mapSecondsToProtoBuffer(value.timebase),
-        );
+        const knownValues = values.filter(value => doesMap(value));
         const now = moment().hour(5); // Always use 5. hour of the day. Less problems with DST.
         let then = moment(now).subtract(2, "month");
         while (now.isSameOrAfter(then)) {
@@ -42,22 +38,13 @@ async function main() {
             day.day = dayString;
             const date = "ISO8601_" + dayString;
             for (const value of knownValues) {
-                const tb = mapper.mapSecondsToProtoBuffer(value.timebase);
-                const vt = mapper.mapGridVisToProtoBuffer(value.valueType.value);
-                const input = mapper.mapInputToProtoBuffer(value.valueType.type);
+                const vs = mapToValueStream(value);
                 const data = await client.values.getValues(project, device, value, date, date);
-                if (tb !== null && vt !== null && input !== null) {
-                    if (data.values.length > 0) {
-                        const vs = new ValueStream();
-                        vs.timebase = tb;
-                        vs.type = vt;
-                        vs.values = data.values.map(eValue => eValue.avg);
-                        vs.input = input;
-                        day.values.push(vs);
-                    }
+                if (data.values.length > 0 && vs) {
+                    vs.values = data.values.map(eValue => eValue.avg);
+                    day.values.push(vs);
                 }
             }
-            console.log(EnergyDay.verify(day));
             if (!EnergyDay.verify(day)) {
                 const writer: Writer = Writer.create();
                 EnergyDay.encode(day, writer);
@@ -65,7 +52,6 @@ async function main() {
                 wStream.write(writer.finish());
                 wStream.close();
             }
-            // console.log(date, data.values.length);
             then = then.add(1, "days");
         }
     }
--- lib/mapper/index.ts
+++ lib/mapper/index.ts
@@ -1,3 +1,33 @@
-export { mapInputToProtoBuffer } from "./InputMapper";
-export { mapSecondsToProtoBuffer } from "./TimeBaseMapper";
-export { mapGridVisToProtoBuffer, mapProtoBufferToGridVis } from "./ValueMapper";
+import { janitza } from "@hg8496/definitions";
+import { IValueDescription } from "@hg8496/gridvis-client/dist/values/IValueDescription";
+import { mapInputToProtoBuffer } from "./InputMapper";
+import { mapSecondsToProtoBuffer } from "./TimeBaseMapper";
+import { mapGridVisToProtoBuffer } from "./ValueMapper";
+import ValueStream = janitza.values.ValueStream;
+import IValueStream = janitza.values.IValueStream;
+
+export { mapInputToProtoBuffer };
+export { mapSecondsToProtoBuffer };
+export { mapGridVisToProtoBuffer };
+
+export function doesMap(value: IValueDescription): boolean {
+    return (
+        mapGridVisToProtoBuffer(value.valueType.value) !== null &&
+        mapSecondsToProtoBuffer(value.timebase) !== null &&
+        mapInputToProtoBuffer(value.valueType.type) !== null
+    );
+}
+
+export function mapToValueStream(value: IValueDescription): IValueStream | null {
+    let result = null;
+    const tb = mapSecondsToProtoBuffer(value.timebase);
+    const vt = mapGridVisToProtoBuffer(value.valueType.value);
+    const input = mapInputToProtoBuffer(value.valueType.type);
+    if (tb !== null && vt !== null && input !== null) {
+        result = new ValueStream();
+        result.timebase = tb;
+        result.type = vt;
+        result.input = input;
+    }
+    return result;
+}
