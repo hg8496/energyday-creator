73601e6b9b8673e4da21ef0c3e3982284cad2dbc
Christian Stolz <hg8496@cstolz.de>
2020-02-06 22:29:31 +0100

    use only smallest timebase

--- lib/index.ts
+++ lib/index.ts
@@ -1,7 +1,7 @@
 import { GridVisClient, IDevice, IProject } from "@hg8496/gridvis-client";
 import { IValueDescription } from "@hg8496/gridvis-client/dist/values/IValueDescription";
 import { EnergyDayCreator } from "./EnergyDayCreator";
-import { doesMap } from "./mapper";
+import { doesMap as DoesMapFilter } from "./mapper";
 
 export interface IProjectDevice {
     project: IProject;
@@ -24,13 +24,42 @@ export async function findDevice(
     return result;
 }
 
+const ValueTypeTimebaseComparator = (vd1: IValueDescription, vd2: IValueDescription) => {
+    if (vd1.valueType.value > vd2.valueType.value) {
+        return 1;
+    } else if (vd1.valueType.value < vd2.valueType.value) {
+        return -1;
+    } else {
+        if (vd1.valueType.type > vd2.valueType.type) {
+            return 1;
+        } else if (vd1.valueType.type < vd2.valueType.type) {
+            return -1;
+        } else {
+            return vd1.timebase - vd2.timebase;
+        }
+    }
+};
+
+const UniqueValueTypeFilter = (vd: IValueDescription, idx: number, arr: IValueDescription[]) => {
+    let result = true;
+    if (idx > 0) {
+        result =
+            arr[idx - 1].valueType.value !== arr[idx].valueType.value ||
+            arr[idx - 1].valueType.type !== arr[idx].valueType.type;
+    }
+    return result;
+};
+
 export async function findNeededValues(
     client: GridVisClient,
     projectName: IProject,
     device: IDevice,
 ): Promise<IValueDescription[]> {
     const values = await client.values.list(projectName, device);
-    return values.filter(value => doesMap(value));
+    return values
+        .filter(DoesMapFilter)
+        .sort(ValueTypeTimebaseComparator)
+        .filter(UniqueValueTypeFilter);
 }
 
 export { EnergyDayCreator };
